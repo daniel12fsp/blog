d1b8890eb1e7e8c349d3794d8ddd8e2c4c5e1ae0
Daniel Pereira <daniel12fsp@yahoo.com.br>
2021-06-26 12:00:38 -0400

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -14,22 +14,34 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-         <link rel="preconnect" href="https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoC1CzjvmyNL4U.woff2" />
-         <link rel="preconnect" href="https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoC1CzjsGyN.woff2" />
-          
-          <style >{ this.props.darkCssContent } </style>
-          {/* TODO: Put this code in good place*/}
-          <style dangerouslySetInnerHTML={{__html: `pre[class*=\"language-\"]  {  background: black !important;      border-radius: 15px;
-   } `}}> 
+          <meta name="commit-hash" content={require('child_process').execSync('git rev-parse HEAD').toString().trim()}/>
+          <meta name="commit-msg" content={require('child_process').execSync('git show -s --format=%s').toString().trim()}/>
 
-          </style>
+          <link rel="preconnect" href="https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoC1CzjvmyNL4U.woff2" />
+          <link rel="preconnect" href="https://fonts.gstatic.com/s/ubuntu/v15/4iCv6KVjbNBYlgoC1CzjsGyN.woff2" />
+          <link rel="preconnect" href="https://www.googletagmanager.com/" />
           
-     
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="yandex-verification" content="79a77ec25a16d825" />
-	    <meta charSet="utf-8"/>
-	    <meta  name="theme-color" content="#20232a" />
-	    <meta name="description" content="Dicas e códigos" />
+            <style >{ this.props.darkCssContent.replace(/\s/g, "").trim() } </style>
+            {/* TODO: Put this code in good place*/}
+            <style dangerouslySetInnerHTML={{__html: `pre[class*=\"language-\"]  {  background: black !important;      border-radius: 15px;
+    } `}}/> 
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="yandex-verification" content="79a77ec25a16d825" />
+            <meta charSet="utf-8"/>
+            <meta  name="theme-color" content="#20232a" />
+            <meta name="description" content="Dicas e códigos" />
+
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WKDLGYWCKD"></script>
+            <script dangerouslySetInnerHTML={{__html:
+               `
+               window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+               gtag('config', 'G-WKDLGYWCKD');
+               `.replace(/\n/g, "").trim()
+            }} />
+           
+           
         </Head>
         <body>
           <Main></Main>
