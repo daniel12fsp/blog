16b28f6a57d82d722e6f43730cf09fbabbe3285d
Daniel Pereira <daniel12fsp@yahoo.com.br>
2021-06-26 11:10:05 -0400

    Improve render time put css in html

--- constants.js
+++ constants.js
@@ -3,3 +3,4 @@ export const postDirectoriesPath = `${rootDirectoryPath}/pages/posts`;
 export const getMetadataFilePath = (postDirectoryPath) =>
   `${postDirectoriesPath}/${postDirectoryPath}/metadata.json`;
 export const getLink = (postDirectory) => `posts/${postDirectory}`;
+export const getDarkCssPath = `${rootDirectoryPath}/assets/dark.min.css`;
--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import fs from "fs";
+import { getDarkCssPath } from "../constants";
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const darkCssContent =  fs.readFileSync(getDarkCssPath, "utf8")
+    // console.log({darkCssContent})
+    return { darkCssContent, ...initialProps };
   }
 
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
+          <style >{ this.props.darkCssContent } </style>
+          {/* TODO: Put this code in good place*/}
+          <style dangerouslySetInnerHTML={{__html: `pre[class*=\"language-\"]  {  background: black !important;      border-radius: 15px;
+   } `}}> 
+
+          </style>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://cdn.jsdelivr.net" />
           <link
             href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap"
             rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/water.css@2.0.0/out/dark.min.css"
           />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="yandex-verification" content="79a77ec25a16d825" />
